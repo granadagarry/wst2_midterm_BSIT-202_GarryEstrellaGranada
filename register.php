b76356174ce47daa1f50d974ad9e8f4a9b64c145
granadagarry <130945676+granadagarry@users.noreply.github.com>
2023-05-15 11:16:40 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Midterm</title>
+    <link rel="stylesheet" href="assets/css/register.css">
+</head>
+<body>
+    <form>
+        <div id="loader1">
+            <h1>Register K Chats</h1>
+        </div>
+        <div>
+            <label for="name"></label>
+            <input type="text" placeholder="Fullname" name="name" id="name">
+            <br>
+        </div>
+        <div>
+            <label for="username"></label>
+            <input type="text" placeholder="Username" name="username" id="username">
+            <br>
+        </div>
+        <div>
+            <label for="password"></label>
+            <input type="password" placeholder="Password" name="password" id="password">
+            <br>
+            <br>
+        </div>
+        <hr>
+            already have account? <a href="login.php">Log In</a>
+        <div>
+            <button type="button">Register</button>
+        </div>
+    </form>
+
+    <script src="assets/js/index.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/login.js"></script>
+</body>
+</html>
