0d6114198475cdfa418654b0d0b1e534d32f1487
granadagarry <130945676+granadagarry@users.noreply.github.com>
2023-05-15 11:20:20 +0800

    Add files via upload

---
+++ class/class.accounts.php
@@ -0,0 +1,28 @@
+<?php
+class Accounts{
+    public function login(){
+        session_start();
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $pdo = new PDO('mysql:host=localhost;dbname=bsit-202-granada-chatroom','root','null');
+
+        $account = $pdo->prepare('SELECT * FROM accounts WHERE username=:username and password=:password');
+        $account->bindValue('username',$username);
+        $account->bindValue('password',$password);
+        $account->execute();
+
+        $data = $account->fetchAll(PDO::FETCH_ASSOC);
+
+        if(count($data) == 1){
+            echo 'success';
+            $_SESSION['auth'] = $data;
+        }else{
+            echo 'error';
+        }
+    }
+
+    public function register(){
+        
+    }
+}
